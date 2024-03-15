9b5ac29959a5b10fd12747e643fd8ff7431ccf18
JEaLiFe Jeaguy <jeanguylanememiaghe@gmail.com>
2024-03-15 15:56:31 +0000

    Changes to be committed: 	modified:   src/app/layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -19,7 +19,7 @@ export const metadata = {
     icon: "/logo.png",
     images:
       "https://images.unsplash.com/photo-1488590528505-98d2b5aba04b?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    url: "https://next-sidebarandnavbar.vercel.app/",
+    url: "https://sidebarandnavbar.vercel.app/",
   },
   twitter: {
     card: "summary_large_image",
