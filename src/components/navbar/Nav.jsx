3804d31c3d9efbb6f9a1c7ddded0f7e2b25b0f83
JEaLiFe Jeaguy <jeanguylanememiaghe@gmail.com>
2024-03-15 15:59:35 +0000

    Changes to be committed: 	modified:   src/components/navbar/Nav.jsx

--- src/components/navbar/Nav.jsx
+++ src/components/navbar/Nav.jsx
@@ -118,14 +118,14 @@ export function NavbarDefault() {
   );
  
   return (
-    <Navbar className="mx-auto max-w-screen-xl px-4 py-2 lg:px-8 lg:py-4 fixed top-5 sm:left-0 lg:left-80 w-full">
+    <Navbar className="mx-auto max-w-screen-xl px-4 py-2 lg:px-8 lg:py-4 fixed top-5 sm:left-0 sm:top-0 lg:left-80 w-full">
       <div className="container mx-auto flex items-center justify-between text-blue-gray-900">
         <Typography
           as="a"
           href="#"
           className="mr-4 cursor-pointer py-1.5 font-medium"
         >
-          Material Tailwind
+          <Jealife></Jealife>
         </Typography>
         <div className="hidden lg:block">{navList}</div>
         <div className="flex items-center gap-x-1">
